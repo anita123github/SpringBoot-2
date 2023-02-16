c5e6e313a3bd630b47ac8fe09175b99da43cc5fc
anita <anubhui27@gmail.com>
2023-02-16 11:33:51 +0530

    Spring boot 2 sirancha proj

---
+++ src/main/java/com/velocity/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.velocity.service;
+
+import java.util.List;
+
+import com.velocity.entity.Employee;
+
+public interface EmployeeService {
+
+	Employee saveEmployee(Employee employee);
+
+	List<Employee> getAllEmployee();
+
+	Employee getEmployeeById(int id);
+
+	int updateEmployee(int id, Employee employee);
+
+String deleteEmployeeById(int id);
+
+}
